d11a82da93f52beabffe459520ddfc8ab7e6a19b
tingis <victor.tingstrom@gmail.com>
2021-03-19 18:14:21 +0100

    Fixes

--- auto_schema/utils.py
+++ auto_schema/utils.py
@@ -27,21 +27,27 @@ def check_column_is_nullable(column: InstrumentedAttribute):
 def map_model(relation_columns: List[RelationshipProperty], data: Union[Dict[str, Any], List[Dict[str, Any]]]):
     base_class = relation_columns[0].class_
 
+    is_list = not isinstance(data, dict)
+    if not is_list:
+        data = [data]
+
     for relation_column in relation_columns:
         key = relation_column.key
 
-        popped = data.pop(key, None)
-        if popped is None or not any(popped):
-            continue
+        for d in data:
+            popped = d.pop(key, None)
+            if popped is None or not any(popped):
+                continue
 
-        mapped_class = relation_column.property.mapper.class_
-        rel_columns = get_columns_of_property_type(mapped_class, prop_type=RelationshipProperty)
+            mapped_class = relation_column.property.mapper.class_
+            rel_columns = get_columns_of_property_type(mapped_class, prop_type=RelationshipProperty)
 
-        if not any(rel_columns):
-            temp = {key: mapped_class(**popped) if isinstance(popped, dict) else [mapped_class(**p) for p in popped]}
-        else:
-            temp = {key: map_model(rel_columns, popped)}
+            if not any(rel_columns):
+                temp = {key: mapped_class(**popped) if isinstance(popped, dict) else [mapped_class(**p) for p in popped]}
+            else:
+                temp = {key: map_model(rel_columns, popped)}
 
-        data.update(temp)
+            d.update(temp)
 
-    return base_class(**data)
+    res = [base_class(**d) for d in data]
+    return res[0] if not is_list else res
